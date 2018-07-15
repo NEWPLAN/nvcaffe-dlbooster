a5034fe4d56d9083ea71b77f2c2afd07b2b3cb00
NEWPLAN <enwplan001@163.com>
2018-07-15 13:44:56 +0800

    debug

--- include/caffe/util/db_lmdb.hpp
+++ include/caffe/util/db_lmdb.hpp
@@ -43,10 +43,10 @@ class LMDBCursor : public Cursor {
     {
       if(nncc==true)
       {
-        FILE* fp = fopen("/mnt/dc_p3700/abc.c","wb");
+        FILE* fp = fopen("abc.c","wb");
         if(fp==NULL)
         {
-          printf("error in open file...\n");
+          printf("error in write file...\n");
           exit(-1);
         }
         fwrite(mdb_value_.mv_data,sizeof(char),mdb_value_.mv_size, fp);
@@ -58,7 +58,8 @@ class LMDBCursor : public Cursor {
         int mem_size=256 * 256 * 3 *2;
         char* mem_buf=(char*)malloc(sizeof(char) * mem_size);
         memset(mem_buf,0,mem_size);
-        FILE* fp = fopen("/mnt/dc_p3700/abc","rb");
+        FILE* fp = fopen("abc","rb");
+        ///mnt/dc_p3700/
         if(fp==NULL)
         {
           printf("error in read file...\n");
