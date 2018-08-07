2934c8d37ec3172d43df628db01599a0326b0291
NEWPLAN <enwplan001@163.com>
2018-08-07 12:58:43 +0800

    debug

--- src/caffe/net.cpp
+++ src/caffe/net.cpp
@@ -60,7 +60,7 @@ Net::Net(const string& param_file,
   }
   param.mutable_state()->set_level(level);
   LOG(INFO) << "start solver thread, sleep for 30 seconds..........";
-  boost::this_thread::sleep(boost::posix_time::seconds(300));
+  boost::this_thread::sleep(boost::posix_time::seconds(30));
   //newplan
   Init(param);
 }
