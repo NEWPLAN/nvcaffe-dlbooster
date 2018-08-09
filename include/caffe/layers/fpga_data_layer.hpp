f004e15600fced75993d014340b60e64df2c13d0
NEWPLAN <enwplan001@163.com>
2018-08-09 16:07:29 +0800

    debug

--- include/caffe/layers/fpga_data_layer.hpp
+++ include/caffe/layers/fpga_data_layer.hpp
@@ -96,10 +96,7 @@ protected:
                 std::this_thread::sleep_for(std::chrono::milliseconds(1)); 
             }
         }
-        else
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        }
+        else std::this_thread::sleep_for(std::chrono::milliseconds(1)); 
     }
   }
 };
