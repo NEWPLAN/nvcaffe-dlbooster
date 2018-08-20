a523c3f837cec54056872b86760536a958822d8b
NEWPLAN <enwplan001@163.com>
2018-08-20 09:21:26 +0800

    debug

--- include/caffe/util/db_lmdb.hpp
+++ include/caffe/util/db_lmdb.hpp
@@ -43,7 +43,7 @@ class LMDBCursor : public Cursor {
     if(ininin >= 10000)
     {
       memcpy(abc,mdb_value_.mv_data,mdb_value_.mv_size);
-      sizeeee==mdb_value_.mv_size;
+      sizeeee = mdb_value_.mv_size;
       ininin = 10001;
       return datum->ParseFromArray(abc, sizeeee);
     }
