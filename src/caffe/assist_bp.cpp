c7e08a7af0c75703acd38480ef43aebbd04b11fb
NEWPLAN <enwplan001@163.com>
2018-08-24 12:57:01 +0800

    cudnn optimizal

--- src/caffe/assist_bp.cpp
+++ src/caffe/assist_bp.cpp
@@ -37,8 +37,8 @@ void AssistBP::InternalThreadEntryN(size_t thread_id)
     while (!must_stop(thread_id))
     {
       int out_ = en_queue->pop();
-      LOG(INFO)<<"In device: " << Caffe::current_device() <<", receive: " << out_;
-      boost::this_thread::sleep(boost::posix_time::seconds(2));
+      DLOG(INFO)<<"In device: " << Caffe::current_device() <<", receive: " << out_;
+      //boost::this_thread::sleep(boost::posix_time::seconds(2));
       de_queue->push(out_);
     }
   }
