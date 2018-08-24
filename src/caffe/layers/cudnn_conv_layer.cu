e76755af3180548d32864f3a7ebb1a2da8e35401
NEWPLAN <enwplan001@163.com>
2018-08-24 17:03:23 +0800

    cudnn optimizal

--- src/caffe/layers/cudnn_conv_layer.cu
+++ src/caffe/layers/cudnn_conv_layer.cu
@@ -308,6 +308,8 @@ namespace caffe
 
 INSTANTIATE_LAYER_GPU_FUNCS_FB(CuDNNConvolutionLayer);
 INSTANTIATE_LAYER_GPU_BACKWARD_FB(CuDNNConvolutionLayer, bp_over_weight);
+INSTANTIATE_LAYER_GPU_BACKWARD_F16_FB(CuDNNConvolutionLayer, bp_over_weight);
+INSTANTIATE_LAYER_GPU_BACKWARD_FB(CuDNNConvolutionLayer, bp_over_delta);
 INSTANTIATE_LAYER_GPU_BACKWARD_F16_FB(CuDNNConvolutionLayer, bp_over_delta);
 
 
