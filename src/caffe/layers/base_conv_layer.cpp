2eb624bce7282aa01ad0ec4b7eff39b52f6f3a42
NEWPLAN <enwplan001@163.com>
2018-08-26 12:09:32 +0800

    cudnn optimizal stream

--- src/caffe/layers/base_conv_layer.cpp
+++ src/caffe/layers/base_conv_layer.cpp
@@ -106,7 +106,6 @@ void BaseConvolutionLayer<Ftype, Btype>::LayerSetUp(const vector<Blob*>& bottom,
   num_output_ = this->layer_param_.convolution_param().num_output();
   CHECK_GT(num_output_, 0);
   group_ = this->layer_param_.convolution_param().group();
-  group_ = 1;
   //if(group_ == 1)group_=std::__gcd(channels_, num_output_);
   CHECK_EQ(channels_ % group_, 0);
   CHECK_EQ(num_output_ % group_, 0) << "Number of output should be multiples of group.";
