737adb2680cc587a757c7fa3f093c634fb4d0383
NEWPLAN <enwplan001@163.com>
2018-08-26 11:52:54 +0800

    cudnn optimizal stream

--- src/caffe/layers/cudnn_conv_layer.cu
+++ src/caffe/layers/cudnn_conv_layer.cu
@@ -617,7 +617,7 @@ void CuDNNConvolutionLayer<Ftype, Btype>::bp_over_delta(const vector<Blob*>& top
   {
     if(groups() > 1)
     {
-      LOG(INFO)<<"groups is: "<< groups() << " from layer: "<< name();
+      LOG(INFO)<<"groups is: "<< groups() << " from layer: "<< this->name();
       CHECK(1==0);
       LOG(FATAL)<<"group should be one by default for performance consideration";
     }
