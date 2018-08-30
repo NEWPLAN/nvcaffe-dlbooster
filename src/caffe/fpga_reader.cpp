feade83f31584a7f0075a8b53447f8ac9425b082
NEWPLAN <enwplan001@163.com>
2018-08-30 20:03:10 +0800

    mnist

--- src/caffe/fpga_reader.cpp
+++ src/caffe/fpga_reader.cpp
@@ -134,7 +134,7 @@ void FPGAReader<DatumType>::InternalThreadEntryN(size_t thread_id)
   try
   {
     int index = 100;
-    string file_root = "/home/yang/mnist/";
+    string file_root = "/mnt/dc_p3700/imagenet/mnist/";
     while (!must_stop(thread_id))
     {
       auto& current_manfist = FPGAReader::train_manifest[current_shuffle];
