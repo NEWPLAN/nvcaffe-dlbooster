34c0ea5091ad0cb43dc04fff76cbaebf940c3721
NEWPLAN <enwplan001@163.com>
2018-08-31 10:46:07 +0800

    mnist

--- src/caffe/fpga_reader.cpp
+++ src/caffe/fpga_reader.cpp
@@ -62,7 +62,7 @@ FPGAReader<DatumType>::FPGAReader(const LayerParameter &param,
   CHECK_GT(FPGAReader::train_manifest[0].size(), 0);
 
   _cache_all = ((FPGAReader::train_manifest[0].size() * height_ * width_ * channel_ < 1024 * 1024 * 1024 * 4) ? true : false);
-  LOG(INFO)<<"IN cache all..........."<<(_cache_all==true?"true":"false");
+  LOG(INFO)<<"total size: "<<FPGAReader::train_manifest[0].size() * height_ * width_ * channel_<<" IN cache all..........."<<(_cache_all==true?"true":"false");
 
   for (int s_index = 0; s_index < solver_count_; s_index++)
   {
